eb740e8b9b7092d828678cb8e55e7c4e50951fe1
Aaron Whang <tlbhibiki@gmail.com>
2019-08-02 16:08:08 -0700

    add ability to add vendors and orders to vendors

---
+++ VaOTracker/Controllers/VendorController.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using VaOTracker.Models;
+
+namespace VaOTracker.Controllers
+{
+  public class VendorController : Controller
+  {
+
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName)
+    {
+      Vendor newVendor = new Vendor(vendorName);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")] 
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrder = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrder);
+      return View(model);
+    }
+
+
+    // This one creates new Items within a given Vendor, not new Categories:
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string orderDescription)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(orderDescription);
+      foundVendor.AddItem(newOrder);
+      List<Order> vendorOrder = foundVendor.Orders;
+      model.Add("orders", vendorOrder);
+      model.Add("vendor", foundVendor);
+      return View("Show", model);
+    }
+
+  }
+}
